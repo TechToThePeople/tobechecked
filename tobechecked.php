49d54ed2afda1bd0fd7b46caa3671de7247fde6f
Xavier Dutoit <civicrm@tttp.eu>
2012-07-24 10:59:13 +0200

    prevented alien invasion. aka do not remember what are the last changes

--- tobechecked.php
+++ tobechecked.php
@@ -2,27 +2,55 @@
 
 require_once 'tobechecked.civix.php';
 
-/**
- * Implementation of hook_civicrm_config
- */
-function tobechecked_civicrm_config(&$config) {
-  _tobechecked_civix_civicrm_config($config);
+
+function civicrm_add_js ($file,$params, $options = array()) {
+  $keys = array_keys ($params);
+  $values = array_values($params);
+  $keys = implode($keys,',');
+  $values = implode($values,',');
+  return "<script>(function($keys){\n".file_get_contents($file)."\n}($values));</script>";
 }
 
-/**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function tobechecked_civicrm_xmlMenu(&$files) {
-  _tobechecked_civix_civicrm_xmlMenu($files);
+
+function tobechecked_civicrm_summary( $contactID, &$content, &$contentPlacement ) {
+  $tag_id =  CRM_Core_BAO_Setting::getItem('eu.tttp.publicautocomplete', 'tag_id');
+  if (!$tag_id) {
+    $tag_id = _tobechecked_init_tag(); 
+  } 
+  $file =  dirname( __FILE__ ) . '/js/tobechecked.js';
+  
+  $content = civicrm_add_js($file, 
+    array(
+      'tag_id' => CRM_Core_BAO_Setting::getItem('eu.tttp.publicautocomplete', 'tag_id'),
+      'contact_id' =>  $contactID
+     )
+  );
+}
+
+function _tobechecked_init_tag () {
+  $name = 'to be checked';
+  $results=civicrm_api("Tag","get", array ('version' => '3','sequential' =>'1', 'name' =>$name));
+  if ($results['count'] == 0) {
+    $results=civicrm_api("Tag","create", array (
+      'version' => 3,
+      'sequential' =>1,
+      'is_reserved' => 1,
+      'name' =>$name, 
+      'description' => "Use this tag to flag a contact in error that needs further investigation.\nUsed by \"to be checked\" extension"
+    ));
+  }
+  CRM_Core_BAO_Setting::setItem($results['id'],'eu.tttp.publicautocomplete', 'tag_id');
+  return $results['id']; 
 }
 
 /**
  * Implementation of hook_civicrm_install
  */
 function tobechecked_civicrm_install() {
-  return _tobechecked_civix_civicrm_install();
+  if (!_tobechecked_init_tag()) {
+    die ("cannot create tag to be checked");
+  }
+//  return _tobechecked_civix_civicrm_install();
 }
 
 /**
